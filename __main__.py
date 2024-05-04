some updates, I don't think the field solver is correct, there should be a negative sign at the gradient

__main__.py
@@ -13,41 +13,43 @@ if __name__ == "__main__":
     from pic.integrator import euler, rk4, leapfrog
     
     # Set up the simulation parameters
-    n_particles = 10
+    n_particles = 1
     n_steps = 100
-    dt = 0.01
-    pusher = leapfrog
+    dt = 1e-7
+    pusher = euler
     
     # Set up grid parameters
-    h = 0.01
-    length = 4.0
-    height = 2.0
+    h = 1e-4
+    length = 0.05
+    height = 0.02
     h_wall = height / 5
     w_wall = length / 5
-    x_wall = 1
+    x_wall = 0.01
     
     # Set electric potentials
-    Vin = 1000
-    Vout = 0
-    Vwall = 3000
+    Vin = 1100
+    Vout = -100
+    Vwall = 1000
 
     # Initialize the objects
     grid = Grid(h, length, height, h_wall, w_wall, x_wall, Vin, Vout, Vwall)
-    particles = Particles(n_particles)
+    particles = Particles(n_particles, height)
     fields = ElectricField(grid)
-    print(particles.positions)
     
-    plt.figure()
-    plt.contourf(grid.Xs, grid.Ys, make_array(grid.get_b(), grid.Nx, grid.Ny))
-    plt.colorbar()
+    # path = []
+    # for _ in range(n_steps):
+    #     particles.push(pusher, fields, dt)
+    #     path.append(np.array(particles.get_position(0)))
+    # path = np.array(path)
+    # plt.figure()
+    # plt.contourf(grid.Xs, grid.Ys, make_array(grid.get_b(), grid.Nx, grid.Ny))
+    # plt.plot(path[:, 0], path[:, 1], linewidth=3, color='r')    
     
-    # import plotly.graph_objects as go
-    # fig = go.Figure(data=[go.Surface(z=make_array(grid.get_b(), grid.Nx, grid.Ny), x=grid.Xs, y=grid.Ys)])
-    # fig.update_layout(width=700, height=700)
-    # fig.show()
+    fields.plot_E_field()
+    fields.plot_contour_V()
     
-    plt.figure()
-    plt.contourf(grid.Xs, grid.Ys, fields.V)
-    plt.colorbar()
+    # plt.figure()
+    # plt.contourf(grid.Xs, grid.Ys, make_array(grid.get_b(), grid.Nx, grid.Ny))
+    # plt.colorbar()
     
     plt.show()

pic/field.py
@@ -3,6 +3,7 @@
 import numpy as np
 from scipy.sparse.linalg import spsolve
 from scipy.interpolate import interp2d
+import matplotlib.pyplot as plt
 from .grid import make_array
 import time
 
@@ -35,11 +36,29 @@ class ElectricField:
 
     def solve_E(self):
         """Solve for the electric field."""
-        Ex, Ey = np.gradient(self.V)
-        Ex = -Ex
-        Ey = -Ey
+        Ex, Ey = np.gradient(self.V, self.grid.h)
+        Ey[0,:] = 0
+        Ey[-1,:] = 0
+        # Ex = -Ex
+        # Ey = -Ey
         return Ex, Ey
     
     def get_field_at(self, x):
         """Return the electric field at a given position."""
-        return np.array([self.fEx[x[0], x[1]], self.fEy[x[0], x[1]]])
+        return np.array([self.fEx(x[0], x[1]), self.fEy(x[0], x[1])]).flatten()
+    
+    def plot_E_field(self):
+        """Plot the electric field."""
+        plt.figure()
+        plt.quiver(self.grid.Xs, self.grid.Ys, self.Ex, self.Ey, color='b')
+        plt.title("Electric Field")
+        plt.xlabel("x")
+        plt.ylabel("y")
+        
+    def plot_contour_V(self):
+        plt.figure()
+        plt.contourf(self.grid.Xs, self.grid.Ys, self.V)
+        plt.colorbar()
+        plt.title("Electric Potential")
+        plt.xlabel("x")
+        plt.ylabel("y")

pic/grid.py
@@ -86,14 +86,12 @@ class Grid:
                     b[id] = self.Vwall
                 # Dirichlet Boundary Condition at bottom
                 elif j == 0:
-                    data.append(np.array([id, id, -2/h**2]))
-                    data.append(np.array([id, id+1, 1/h**2]))
-                    data.append(np.array([id, id-1, 1/h**2]))
+                    data.append(np.array([id, id, 1]))
+                    data.append(np.array([id, id+self.Ny, -1]))
                 # Dirichlet Boundary Condition at top
                 elif j == self.Ny - 1:
-                    data.append(np.array([id, id, -2/h**2]))
-                    data.append(np.array([id, id+1, 1/h**2]))
-                    data.append(np.array([id, id-1, 1/h**2]))
+                    data.append(np.array([id, id, 1]))
+                    data.append(np.array([id, id-self.Ny, -1]))
                 # Rest of the domain with standar Laplacian
                 else:
                     data.append(np.array([id, id, -4/h**2]))

pic/particle.py
@@ -2,20 +2,26 @@
 
 import numpy as np
 
-Q = 1.60217662e-19
+# Single Xenon ion charge and mass
+Q =  1.60217657e-19
+M = 131.293*1.66053892*1e-27
 
 class Particles:
     """Class representing a collection of particles in a PIC simulation."""
-    def __init__(self, n_particles):
+    def __init__(self, n_particles, height):
         """
         Initialize the particle object with random positions and velocities.
 
         Args:
             n_particles (int): Number of particles to create.
         """
-        self.num = n_particles
+        self.num = n_particles        
         self.positions = np.random.rand(n_particles, 2)
+        self.positions[:, 0] = 0
+        self.positions[:, 1] *= height
         self.velocities = np.random.rand(n_particles, 2)
+        self.velocities[:, 0] = abs(self.velocities[:, 0])
+        self.velocities[:, 1] = 0
 
     def push(self, pusher, electric_field, dt):
         """
@@ -30,8 +36,12 @@ class Particles:
         for i in range(self.num):
             x = self.positions[i]
             v = self.velocities[i]
-            a = lambda pos: Q*electric_field.get_field_at(pos);
+            # print("x = ", x)
+            # print("v = ", v)
+            a = lambda pos: Q*electric_field.get_field_at(pos)/M;
             x_new, v_new = pusher(x, v, a, dt)
+            # print("x_new = ", x_new)    
+            # print("v_new = ", v_new)
             self.positions[i] = x_new
             self.velocities[i] = v_new
 