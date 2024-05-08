438db8470c73832bd814bd8b51dde2d64f15e557
Dario Panici <dpanici@princeton.edu>
2024-05-07 22:22:25 -0400

    add energy loss scale plot

--- pic/__main__.py
+++ pic/__main__.py
@@ -51,52 +51,89 @@ if __name__ == "__main__":
     }
     print(f"Using {methods} method for integration")
 
+    if len(methods) > 1:
+        raise ValueError("please only run with one method on this branch!")
+
     fig, axs = plt.subplots(1, 2, figsize=(10, 5))
     colors = ["red", "blue", "green"]
     linestyles = ["-", "-", "--"]
-    for i, method in enumerate(methods):
-        pusher = pushers[method]
-        # Initialize the objects
-        grid = Grid(h, length, height, h_wall, w_wall, x_wall, Vin, Vout, Vwall)
-        particles = Particles(n_particles, height, v0)
-        fields = ElectricField(grid)
+    fig_conv, axs_conv = plt.subplots(1, 1, figsize=(5, 5))
+    E_diffs = []
+    dts = []
+    for dt in [dt, dt / 10, dt / 100]:
+        for i, method in enumerate(methods):
+            pusher = pushers[method]
+            # Initialize the objects
+            grid = Grid(h, length, height, h_wall, w_wall, x_wall, Vin, Vout, Vwall)
+            particles = Particles(n_particles, height, v0)
+            fields = ElectricField(grid)
 
-        paths = {}
-        energies = {}
-        for k in range(n_steps):
-            if k == 0:
+            paths = {}
+            energies = {}
+            for k in range(n_steps):
+                if k == 0:
+                    for j in range(n_particles):
+                        paths[j] = [np.array(particles.get_position(j))]
+                        r = particles.get_position(j)
+                        v = particles.get_velocity(j)
+                        K = 0.5 * particles.M * np.linalg.norm(v) ** 2
+                        U = particles.Q * fields.get_potential_at(r)
+                        energies[j] = [K + U]
+
+                particles.push(pusher, fields, dt, grid)
                 for j in range(n_particles):
-                    paths[j] = [np.array(particles.get_position(j))]
+                    paths[j].append(np.array(particles.get_position(j)))
                     r = particles.get_position(j)
                     v = particles.get_velocity(j)
                     K = 0.5 * particles.M * np.linalg.norm(v) ** 2
                     U = particles.Q * fields.get_potential_at(r)
-                    energies[j] = [K + U]
+                    energies[j].append(K + U)
 
-            particles.push(pusher, fields, dt, grid)
+                if r[0] >= length:
+                    break
+
             for j in range(n_particles):
-                paths[j].append(np.array(particles.get_position(j)))
-                r = particles.get_position(j)
-                v = particles.get_velocity(j)
-                K = 0.5 * particles.M * np.linalg.norm(v) ** 2
-                U = particles.Q * fields.get_potential_at(r)
-                energies[j].append(K + U)
+                E = energies[j]
+                axs[0].plot(
+                    np.arange(len(E[:-2])),
+                    E[:-2],
+                    label=method,
+                    linestyle=linestyles[i],
+                )
+                axs[0].set_xlabel("step number", fontsize=13)
+                axs[0].set_ylabel("Total Energy", fontsize=13)
+                axs[0].set_title("Energy with the wall", fontsize=14)
+                axs[0].legend()
+                plt.tight_layout()
 
-            if r[0] >= length:
-                break
+                E_diffs.append(E[0] - E[-3])
+                dts.append(dt)
 
-        for j in range(n_particles):
-            E = energies[j]
-            axs[0].plot(np.arange(len(E[:-2])), E[:-2], label=method, linestyle=linestyles[i])
-            axs[0].set_xlabel("step number", fontsize=13)
-            axs[0].set_ylabel("Total Energy", fontsize=13)
-            axs[0].set_title("Energy with the wall", fontsize=14)
-            axs[0].legend()
-            plt.tight_layout()
+            for j in range(n_particles):
+                paths[j] = np.array(paths[j])
+                axs[1].plot(
+                    paths[j][:-2, 0], paths[j][:-2, 1], linewidth=3, color=colors[i]
+                )
 
-        for j in range(n_particles):
-            paths[j] = np.array(paths[j])
-            axs[1].plot(paths[j][:-2, 0], paths[j][:-2, 1], linewidth=3, color=colors[i])
+    axs_conv.scatter(dts, E_diffs)
+    axs_conv.set_xscale("log")
+    axs_conv.set_yscale("log")
+    p = np.polyfit(x=dts, y=E_diffs, deg=2)
+    print(p)
+    fit_y = np.polyval(p, np.array([dts[0], dts[-1]]))
+    axs_conv.plot(
+        [dts[0], dts[-1]],
+        fit_y,
+        "--",
+        label=r"degree 2 polynomial fit $\sim \Delta t^2$",
+    )
+    axs_conv.set_xscale("log")
+    axs_conv.set_yscale("log")
+    axs_conv.legend()
+    axs_conv.set_xlabel("dt (s)")
+    axs_conv.set_ylabel("Initial E - Final E (J))")
+    axs_conv.set_title("Euler Energy Loss versus timestep size")
+    fig_conv.savefig("Euler_energy_loss_co")
 
     p = axs[1].contourf(fields.grid.Xs, fields.grid.Ys, fields.V, 20)
     plt.gca().add_patch(
